a5bd9cc406c0f664180028054f6fbe9343ff177a
james <j@star.shopping>
2020-07-25 21:23:41 +0100

    ✍ Added ".add" command

--- cogs/add_command.py
+++ cogs/add_command.py
@@ -1,23 +1,50 @@
-import discord, asyncio, datetime
+import discord, asyncio, json
 from discord.ext import commands
 
-class AddCommand(commands.Cog):
+class Add(commands.Cog):
     @commands.command()
-    async def rEmbed(self, ctx):
+    async def add(self, ctx, *args):
         if not ctx.message.author.guild_permissions.administrator:
             return
 
-        embed = discord.Embed(colour=0x202225, description = """
-            To get your config, type .redeem following your purchase ID
+        length = len(args)
 
-            ```.redeem XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX```
-            If you have any issue redeeming your config please contact the server owner""")
+        with open('products.json') as f:
+            message = f.read()
+            f.close()           
         
-        try:
+        if length == 0:
+            embed = discord.Embed(colour=0x202225, description = f"""
+            ```json
+
+            {message}
+            ```
+
+            To set a product ID to a role please use: .add [Product ID] [Role name]. 
+            Please note that both of these values are case sensitive.""")
+
             await ctx.message.channel.send(embed=embed)
-        except:
-            print('Error trying to send the embed command - maybe the bot doesn\'t have permission?')
+            return
+
+        if length != 2:
+            await ctx.message.channel.send('The correct usage is .add [Product ID] [Role name]. These values are case sensitive.')
+            return
+
+        message = json.loads(message)
+
+        message[args[0]] = args[1]
+
+        with open('products.json', 'w') as f:
+            f.write(json.dumps(message, indent=2))
+            f.close()
+
+        embed = discord.Embed(colour=0x202225, description = 'Updated products.json successfully')
+
+        await ctx.message.channel.send(embed=embed)
+        return
+
+        
 
 def setup(bot):
-    bot.add_cog(AddCommand(bot))
-    print('AddCommand cog has been loaded')
+    bot.add_cog(Add(bot))
+    print('Add cog has been loaded')
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord, json
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='.', case_insensitive=True)
-extensions = ['cogs.redeem_embed', 'cogs.on_message', 'cogs.redeem_command']
+extensions = ['cogs.redeem_embed', 'cogs.on_message', 'cogs.redeem_command', 'cogs.add_command']
 
 with open('settings.json') as f:
   data = json.load(f)
